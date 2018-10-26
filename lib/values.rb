5265aee301b78cfb0d11b55dff0935116c10c830
Paula McKinley <pmknyc@optimum.net>
2018-10-26 03:53:49 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,9 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  groceries.values
+#  groceries.each do |grocery_type, item_array|
+#    grocery_type.values
+  binding.pry
+  end.flatten
 end
